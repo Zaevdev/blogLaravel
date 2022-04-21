ef7e1525cd3525edea5070e4c616ea1d3e6566d5
Zaevdev <zaevdev@gmail.com>
2022-04-21 17:10:10 +0300

    Change work with the database when creating and updating a post in a transaction

--- app/Http/Service/PostService.php
+++ app/Http/Service/PostService.php
@@ -3,15 +3,20 @@
 namespace App\Http\Service;
 
 use App\Models\Post;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
 
 class PostService
 {
     public function store($data)
     {
-        try {
-            $tagsId = $data['tags_id'];
-            unset($data['tags_id']);
+        Db::transaction(function () use ($data): void {
+            if (isset($data['tags_id'])) {
+                $tagsId = $data['tags_id'];
+                unset($data['tags_id']);
+            } else {
+                $tagsId = null;
+            }
 
             if (isset($data['preview_image'])) {
                 $data['preview_image'] = Storage::disk('public')->put('/image', $data['preview_image']);
@@ -22,14 +27,32 @@ class PostService
 
             $post = Post::firstOrCreate($data);
             $post->tags()->attach($tagsId);
-        } catch (\Exception $exception) {
-            abort(500);
-        }
+        });
+
+//        try {
+//            Db::beginTransaction();
+//            $tagsId = $data['tags_id'];
+//            unset($data['tags_id']);
+//
+//            if (isset($data['preview_image'])) {
+//                $data['preview_image'] = Storage::disk('public')->put('/image', $data['preview_image']);
+//            }
+//            if (isset($data['main_image'])) {
+//                $data['main_image'] = Storage::disk('public')->put('/image', $data['main_image']);
+//            }
+//
+//            $post = Post::firstOrCreate($data);
+//            $post->tags()->attach($tagsId);
+//            Db::commit();
+//        } catch (\Exception $exception) {
+//            Db::rollBack();
+//            abort(500);
+//        }
     }
 
     public function update($data, $post)
     {
-        try {
+        Db::transaction(function () use ($data, $post): void {
             if (isset($data['tags_id'])) {
                 $tagsId = $data['tags_id'];
                 unset($data['tags_id']);
@@ -47,9 +70,7 @@ class PostService
             $post->update($data);
 
             $post->tags()->sync($tagsId);
-        } catch (\Exception $exception) {
-            abort(500);
-        }
+        });
 
         return $post;
     }
